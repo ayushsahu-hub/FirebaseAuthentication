8a47e82fe674f64e3a1a7476b4ada575da70731b
ayushsahu-hub <81520698+ayushsahu-hub@users.noreply.github.com>
2024-10-01 20:05:53 +0530

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export let config = {
     API_URL : 'https://authangular-de072-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    API_KEY :'AIzaSyAuPGuiPZQU68vuOP_SpusuiDSoAjbUyGE'
+    API_KEY :'paste-your-key-here'
 }
